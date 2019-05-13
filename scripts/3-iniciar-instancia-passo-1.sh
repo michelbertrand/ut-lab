b70f0408f2750683029f437b0d2d1bddf57c0f28
Michel Bertrand <michel.bertrand@fluig.com>
2019-05-13 16:35:09 -0300

    Criação do repositório.

---
+++ scripts/3-iniciar-instancia-passo-1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Criando uma rede virtual no Docker
+docker network create fluig-docker-network;
+
+# Iniciando o servidor do Realtime
+docker run --name realtime-server --network fluig-docker-network --rm \
+     -p 7777:7777 -p 8888:8888 \
+     -d docker.fluig.com/snapshot/fluig/node:c37ea34;
+
+# Iniciando o servidor de indexação e busca
+docker run --name index-server --rm --network fluig-docker-network \
+     -p 8983:8983 -d docker.fluig.com/snapshot/fluig/solr:c37ea34;
+
+# Iniciando o servidor de banco de dados MySQL
+docker run --name database-server --network fluig-docker-network –rm \
+     -p 3306:3306 \
+     -e MYSQL_ROOT_PASSWORD=YuCBDOZnqE5vL \
+     -e MYSQL_USER=fluig \
+     -e MYSQL_PASSWORD=OLyVu1gIwWmEr \
+     -e MYSQL_DATABASE=fluig \
+     -d docker.fluig.com/fluig/mysql;
---
+++ scripts/4-iniciar-instancia-passo-2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Iniciando o servidor do fluig
+docker run --name fluig-server --rm --network fluig-docker-network \
+     -p 8080:8080 -v /opt/volume:/opt/volume -v /opt/fluig1/:/opt/fluig/appserver/domain/servers/fluig1 \
+	 -e dbType=mysql \
+	 -e dbServer=database-server
+	 -e dbPort=3306 \
+	 -e dbUser=fluig \
+	 -e dbPassword=OLyVu1gIwWmEr \
+	 -e dbName=fluig \
+	 -e solrUrl=http://index-server:8983/solr \
+	 -e solrIsRemote=true \
+	 -e solrIsCloud=false \
+	 -e lsHost=10.80.129.242 \
+	 -e lsPort=5555
+	 -e fluigHost=fluig-server \
+	 -e fluigPort=8080 \
+	 -e fluigUrl=http://code<numero>.fluig.io:8080 \
+	 -e fluigXms=2g \
+	 -e fluigXmx=3g \
+	 -e smtpHost=fluig-server \
+	 -e smtpPort=25 \
+	 -e nodeChatAddress=realtime-server:7777 \
+	 -e nodeRealtimeAddress=realtime-server:8888 \
+	 -d docker.fluig.com/snapshot/fluig/fluig:c37ea34;
