e94da6baa7e4ca878d91bd127e64a16be4aa7533
Michel Bertrand <michel.bertrand@fluig.com>
2019-05-14 10:26:26 -0300

    Formação da mensagem de inicialização do banco de dados.

--- scripts/3-iniciar-instancia-passo-1.sh
+++ scripts/3-iniciar-instancia-passo-1.sh
@@ -19,7 +19,7 @@ docker run --name index-server --rm --network fluig-docker-network \
      -p 8983:8983 -d docker.fluig.com/snapshot/fluig/solr:c37ea34;
 
 # Iniciando o servidor de banco de dados MySQL
-echo 'Iniciando o servidor de banco de dados';
+echo 'Iniciando o servidor de banco de dados ...';
 docker run --name database-server --network fluig-docker-network --rm \
      -p 3306:3306 \
      -e MYSQL_ROOT_PASSWORD=YuCBDOZnqE5vL \
