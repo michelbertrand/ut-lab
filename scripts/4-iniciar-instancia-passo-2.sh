c91560137775debfcb6b4f5f8c3011c5c913abe9
Michel Bertrand <michel.bertrand@fluig.com>
2019-05-20 17:45:42 -0300

    Remove servidor do ls.

--- scripts/4-iniciar-instancia-passo-2.sh
+++ scripts/4-iniciar-instancia-passo-2.sh
@@ -12,8 +12,8 @@ docker run --name fluig-server --network fluig-docker-network \
    -e solrUrl=http://index-server:8983/solr \
    -e solrIsRemote=true \
    -e solrIsCloud=false \
-   -e lsHost=localhost \
-   -e lsPort=5555 \
+   -e lsHost=$lsHost \
+   -e lsPort=$lsPort \
    -e fluigHost=fluig-server \
    -e fluigPort=8080 \
    -e fluigUrl=$fluigURL \
