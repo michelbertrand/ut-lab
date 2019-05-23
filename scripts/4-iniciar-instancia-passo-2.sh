8008b61d6140568000e6bc69a90096e3cdcf42c2
Michel Bertrand <michel.bertrand@fluig.com>
2019-05-23 14:11:29 -0300

    Correção dos endereços para usar a rede do host do container.

--- scripts/4-iniciar-instancia-passo-2.sh
+++ scripts/4-iniciar-instancia-passo-2.sh
@@ -4,23 +4,23 @@ echo 'Iniciando o servidor do fluig ...';
 docker run --name fluig-server \
    -p 80:80 -p 8080:8080 -p 5555:5555 -v /opt/volume:/opt/volume \
    -e dbType=mysql \
-   -e dbServer=database-server \
+   -e dbServer=localhost \
    -e dbPort=3306 \
    -e dbUser=fluig \
    -e dbPassword=OLyVu1gIwWmEr \
    -e dbName=fluig \
-   -e solrUrl=http://index-server:8983/solr \
+   -e solrUrl=http://localhost:8983/solr \
    -e solrIsRemote=true \
    -e solrIsCloud=false \
    -e lsHost=ls.fluig.io \
    -e lsPort=5555 \
-   -e fluigHost=fluig-server \
+   -e fluigHost=localhost \
    -e fluigPort=8080 \
    -e fluigUrl=http://utlab.fluig.io:8080 \
    -e fluigXms=2g \
    -e fluigXmx=3g \
-   -e smtpHost=fluig-server \
+   -e smtpHost=localhost \
    -e smtpPort=25 \
-   -e nodeChatAddress=realtime-server:7777 \
-   -e nodeRealtimeAddress=realtime-server:8888 \
+   -e nodeChatAddress=localhost:7777 \
+   -e nodeRealtimeAddress=localhost:8888 \
    -d docker.fluig.com/snapshot/fluig/fluig:c37ea34;
