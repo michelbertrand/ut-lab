65a10b915d7bdee59bf2ca33b670e262acaecf2b
Michel Bertrand <michel.bertrand@fluig.com>
2019-05-23 14:04:39 -0300

    Remoção da rede usada pelos containers.

--- scripts/3-iniciar-instancia-passo-1.sh
+++ scripts/3-iniciar-instancia-passo-1.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
-# Criando uma rede virtual no Docker
-dockerNetwork=$(docker network ls | grep fluig-docker-network | awk '{ print $2 }');
-if [ $dockerNetwork = "fluig-docker-network" ]; then
-  echo 'A rede do docker está disponível: fluig-docker-network.';
-else
-  docker network create fluig-docker-network;
-  echo 'A rede do docker foi criada com sucesso.';
-fi
 # Iniciando o servidor do Realtime
 echo 'Iniciando o servidor Realtime ...';
-docker run --name realtime-server --network fluig-docker-network \
+docker run --name realtime-server \
      -p 7777:7777 -p 8888:8888 \
      -d docker.fluig.com/snapshot/fluig/node:c37ea34;
 # Iniciando o servidor de indexação e busca
 echo 'Iniciando o servidor de busca e indexação ...';
-docker run --name index-server --network fluig-docker-network \
+docker run --name index-server \
      -p 8983:8983 -d docker.fluig.com/snapshot/fluig/solr:c37ea34;
 # Iniciando o servidor de banco de dados MySQL
 echo 'Iniciando o servidor de banco de dados ...';
-docker run --name database-server --network fluig-docker-network \
+docker run --name database-server \
      -p 3306:3306 \
      -e MYSQL_ROOT_PASSWORD=YuCBDOZnqE5vL \
      -e MYSQL_USER=fluig \
--- scripts/4-iniciar-instancia-passo-2.sh
+++ scripts/4-iniciar-instancia-passo-2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Iniciando o servidor do fluig
 echo 'Iniciando o servidor do fluig ...';
-docker run --name fluig-server --network fluig-docker-network \
+docker run --name fluig-server \
    -p 80:80 -p 8080:8080 -p 5555:5555 -v /opt/volume:/opt/volume \
    -e dbType=mysql \
    -e dbServer=database-server \
