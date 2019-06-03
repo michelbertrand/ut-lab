e4b6e6efcb19e3cb7c82ce34d6f1b4a4d304fd31
Michel Bertrand <michel.bertrand@fluig.com>
2019-06-03 09:56:39 -0300

    Correção no script de remoção dos volumes orfãos.

--- scripts/utils/4-remover-volumes-orfaos.sh
+++ scripts/utils/4-remover-volumes-orfaos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ### Remove os volumes orfaos criados por containers já excluídos
 echo "Removendo volumes orfãos ...";
-docker rm $(docker ps -a | awk '{if (NR!=1) {{print $1}}}');
+docker volume rm $(docker volume ls -qf dangling=true)
 echo "Pronto!";
